622a0284e65c91fe443a2c52d83935b26db74b73
Yuri Sousa <ysousa.dev@gmail.com>
2024-09-11 17:45:32 -0300

    feat: :sparkles: adiciona componente de Textarea que vou usar em uma página...

---
+++ src/components/Textarea/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from "./styles";
+
+export function Textarea({...rest}) {
+    return(
+        <Container {...rest}>
+             
+        </Container>
+    )
+}
---
+++ src/components/Textarea/styles.js
@@ -0,0 +1,18 @@
+import { styled } from 'styled-components'
+
+export const Container = styled.textarea`
+    font-size: 16px;
+    background-color: #262529;
+    padding: 16px;
+
+    width: 100%;
+    height: 250px;
+    resize: none;
+
+    color: #F4EDE8;
+    
+    border: none;
+    border-radius: 10px;
+
+    margin: 40px 0;
+`
